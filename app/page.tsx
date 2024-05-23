d5859176cc0479c796130436730d0d4692e65301
Vex <masonbesmer@gmail.com>
2024-05-22 23:52:26 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1>Hello I'm sasdf!</h1>
+        <h1>Hello Im sasdf!</h1>
       </div>
     </main>
   );
